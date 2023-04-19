05ff00e3cb9f7e7afd3f0d09f9c2949f55438407
Raju Islam <bdrajuislam246@gmail.com>
2023-04-19 14:57:52 +0600

    load data in ui

--- src/App.js
+++ src/App.js
@@ -22,8 +22,19 @@ function Loadcountry(){
     <div>
       <h1>Visit All country over the World!!!</h1>
       <h3>{countries.length}</h3>
+      {
+        countries.map(country=> <Country name={country.name.common} population={country.population} map={country.flags}></Country>)
+      }
     </div>
   )
 }
-
+ function Country(props){
+  return (
+    <div>
+        <h2>Name: {props.name}</h2>
+        <h4>Population: {props.population}</h4>
+        
+    </div>
+  )
+ }
 export default App;
